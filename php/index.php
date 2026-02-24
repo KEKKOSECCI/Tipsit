7b03372f9b2ec9f6c85313e5404c1ae7e05f3198
lai2-24 <lai2-24@itismeucci.com>
2026-02-24 12:15:37 +0100

    Primo Commit

---
+++ php/index.php
@@ -0,0 +1,20 @@
+<?php
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/controllers/AlunniController.php';
+
+$app = AppFactory::create();
+
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+$app->get('/alunni', "AlunniController:index");
+
+
+$app->run();
